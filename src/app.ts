4173f5bbedf7abcae46c6098706f3190285f0f8b
Muhammad SA <MuhammadSA211999>
2023-07-31 10:23:16 +0600

    setup eslint

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ app.use(cors())
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
 
+//const env=app.get("env") //development deafult
+//const envObj=process.env //{.env file er sob variable and node er default variable}
+
 // application routes
 // user related routes
 app.use('api/v1/user', userRouter)
